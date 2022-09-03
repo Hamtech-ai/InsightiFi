018b66defcd5d9d6efa478f5998a7ed12f9e2c86
AlirezaHanifi <hanifi1375@yahoo.com>
2022-09-03 19:50:37 +0430

    SMA added to indicator features

--- indicator_features.py
+++ indicator_features.py
@@ -3,11 +3,45 @@ import pandas as pd
 import ta
 #import copy
 
+# Trend Indicator: Simple Moving Average (SMA)
+def SMA(df):
+
+    df['SMA10d'] = ta.trend.SMAIndicator(df['adjClose'], window = 10).sma_indicator()
+    df['SMA20d'] = ta.trend.SMAIndicator(df['adjClose'], window = 20).sma_indicator()
+    df['last_SMA20d'] = ta.trend.SMAIndicator(df['adjClose'], window = 20).sma_indicator().shift(1)
+    df['SMA30d'] = ta.trend.SMAIndicator(df['adjClose'], window = 30).sma_indicator()
+    df['SMA50d'] = ta.trend.SMAIndicator(df['adjClose'], window = 50).sma_indicator()
+    df['last_SMA50d'] = ta.trend.SMAIndicator(df['adjClose'], window = 50).sma_indicator().shift(1)
+    df['SMA80d'] = ta.trend.SMAIndicator(df['adjClose'], window = 80).sma_indicator()
+
+    # buy signal
+    df['RSI_buy'] = np.nan
+    df.loc[(df['last_SMA20d'] < df['last_SMA50d']) & (df['SMA20d'] > df['SMA50d']), 'SMA_buy'] = 1
+    # sell signal
+    df.loc[(df['last_SMA20d'] > df['last_SMA50d']) & (df['SMA20d'] < df['SMA50d']), 'SMA_buy'] = 0
+
+    df['SMA_position'] = df['SMA_buy'].fillna(method = 'ffill')
+
+    SMAdf =  pd.DataFrame(
+        {
+        'SMA10d': df['SMA10d'],
+        'SMA20d': df['SMA20d'],
+        'last_SMA20d': df['last_SMA20d'],
+        'SMA30d': df['SMA30d'],
+        'SMA50d': df['SMA50d'],
+        'last_SMA50d': df['last_SMA50d'],
+        'SMA80d': df['SMA80d'],
+        'SMA_buy': df['SMA_buy'],
+        'SMA_position': df['SMA_position']
+        }
+    )
+
+    return SMAdf
+
+
+# Momentum Indicator: Relative Strength Index (RSI)
 def RSI(df, window = 15):
 
-    if 'adjClose' not in df.columns:
-        ValueError('your dataframe does not have a column named adjClose')
-    
     overBought = 70
     overSold = 30
 
@@ -16,12 +50,9 @@ def RSI(df, window = 15):
 
     # buy signal
     df['RSI_buy'] = np.nan
-    df.loc[(df['last_RSI'] < overBought) & (df['RSI'] > overBought), 'RSI_buy'] = 1
     df.loc[(df['last_RSI'] < overSold) & (df['RSI'] > overSold), 'RSI_buy'] = 1
-    
     # sell signal
     df.loc[(df['last_RSI'] > overBought) & (df['RSI'] < overBought), 'RSI_buy'] = 0 
-    df.loc[(df['last_RSI'] > overSold) & (df['RSI'] < overSold), 'RSI_buy'] = 0
 
     df['RSI_position'] = df['RSI_buy'].fillna(method = 'ffill')
 
@@ -35,3 +66,4 @@ def RSI(df, window = 15):
     )
 
     return RSIdf
+    
