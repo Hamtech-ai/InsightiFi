f7e5e6ea49c39214549e0e66d8c488264a19c1e5
AlirezaHanifi <hanifi1375@yahoo.com>
2022-09-24 19:12:23 +0330

    features added to training model

--- feature_extraction/daily_features.py
+++ feature_extraction/daily_features.py
@@ -4,10 +4,23 @@ import numpy as np
 import pandas as pd
 
 
+## shift data for 1 day ##
+##########################
+def shift_data(stock):
+    df = stock.copy()
+
+    df = pd.concat(
+        [
+            df.shift(1).drop(columns = 'date').add_prefix('last1d_')
+        ], axis = 1  
+    )
+
+    return df
+
 ## calender features ##
 #######################
 def calender_features(stock):
-    df = stock
+    df = stock.copy()
     
     return_df = pd.DataFrame(
         {
@@ -26,87 +39,82 @@ def calender_features(stock):
     
     return return_df
 
+# ## candlestick features ##
+# ##########################
+# def candle_based(stock):
+#     df = stock
 
-## candlestick features ##
-##########################
-def candle_based(stock):
-    df = stock
+#     return_df = pd.DataFrame(
+#         {
+#         'max_price7d': df['high'].rolling(5).max(),
+#         'min_price7d': df['low'].rolling(5).min(),
+#         'first_price7d': df['open'].rolling(5).agg(lambda rows: rows[0]),
+#         'last_price7d': df['close'].rolling(5).agg(lambda rows: rows[-1]),
+#         'yesterday_price7d': df['yesterday'].rolling(5).agg(lambda rows: rows[0]),
+#         'max_price14d': df['high'].rolling(10).max(),
+#         'min_price14d': df['low'].rolling(10).min(),
+#         'first_price14d': df['open'].rolling(10).agg(lambda rows: rows[0]),
+#         'last_price14d': df['close'].rolling(10).agg(lambda rows: rows[-1]),
+#         'yesterday_price14d': df['yesterday'].rolling(10).agg(lambda rows: rows[0]),
+#         'max_price21d': df['high'].rolling(15).max(),
+#         'min_price21d': df['low'].rolling(15).min(),
+#         'first_price21d': df['open'].rolling(15).agg(lambda rows: rows[0]),
+#         'last_price21d': df['close'].rolling(15).agg(lambda rows: rows[-1]),
+#         'yesterday_price21d': df['yesterday'].rolling(15).agg(lambda rows: rows[0]),
+#         'max_price30d': df['high'].rolling(23).max(),
+#         'min_price30d': df['low'].rolling(23).min(),
+#         'first_price30d': df['open'].rolling(23).agg(lambda rows: rows[0]),
+#         'last_price30d': df['close'].rolling(23).agg(lambda rows: rows[-1]),
+#         'yesterday_price30d': df['yesterday'].rolling(23).agg(lambda rows: rows[0]),
+#         'max_price60d': df['high'].rolling(44).max(),
+#         'min_price60d': df['low'].rolling(44).min(),
+#         'first_price60d': df['open'].rolling(44).agg(lambda rows: rows[0]),
+#         'last_price60d': df['close'].rolling(44).agg(lambda rows: rows[-1]),
+#         'yesterday_price60d': df['yesterday'].rolling(44).agg(lambda rows: rows[0]),
+#         'max_price90d': df['high'].rolling(70).max(),
+#         'min_price90d': df['low'].rolling(70).min(),
+#         'first_price90d': df['open'].rolling(70).agg(lambda rows: rows[0]),
+#         'last_price90d': df['close'].rolling(70).agg(lambda rows: rows[-1]),
+#         'yesterday_price90d': df['yesterday'].rolling(70).agg(lambda rows: rows[0])
+#         }
+#     )
 
+#     return return_df
+    
+    
+def ret_based(stock):
+    df = stock.copy()
     return_df = pd.DataFrame(
         {
-        'max_price7d': df['high'].rolling(5).max(),
-        'min_price7d': df['low'].rolling(5).min(),
-        'first_price7d': df['open'].rolling(5).agg(lambda rows: rows[0]),
-        'last_price7d': df['close'].rolling(5).agg(lambda rows: rows[-1]),
-        'yesterday_price7d': df['yesterday'].rolling(5).agg(lambda rows: rows[0]),
-        'max_price14d': df['high'].rolling(10).max(),
-        'min_price14d': df['low'].rolling(10).min(),
-        'first_price14d': df['open'].rolling(10).agg(lambda rows: rows[0]),
-        'last_price14d': df['close'].rolling(10).agg(lambda rows: rows[-1]),
-        'yesterday_price14d': df['yesterday'].rolling(10).agg(lambda rows: rows[0]),
-        'max_price21d': df['high'].rolling(15).max(),
-        'min_price21d': df['low'].rolling(15).min(),
-        'first_price21d': df['open'].rolling(15).agg(lambda rows: rows[0]),
-        'last_price21d': df['close'].rolling(15).agg(lambda rows: rows[-1]),
-        'yesterday_price21d': df['yesterday'].rolling(15).agg(lambda rows: rows[0]),
-        'max_price30d': df['high'].rolling(23).max(),
-        'min_price30d': df['low'].rolling(23).min(),
-        'first_price30d': df['open'].rolling(23).agg(lambda rows: rows[0]),
-        'last_price30d': df['close'].rolling(23).agg(lambda rows: rows[-1]),
-        'yesterday_price30d': df['yesterday'].rolling(23).agg(lambda rows: rows[0]),
-        'max_price60d': df['high'].rolling(44).max(),
-        'min_price60d': df['low'].rolling(44).min(),
-        'first_price60d': df['open'].rolling(44).agg(lambda rows: rows[0]),
-        'last_price60d': df['close'].rolling(44).agg(lambda rows: rows[-1]),
-        'yesterday_price60d': df['yesterday'].rolling(44).agg(lambda rows: rows[0]),
-        'max_price90d': df['high'].rolling(70).max(),
-        'min_price90d': df['low'].rolling(70).min(),
-        'first_price90d': df['open'].rolling(70).agg(lambda rows: rows[0]),
-        'last_price90d': df['close'].rolling(70).agg(lambda rows: rows[-1]),
-        'yesterday_price90d': df['yesterday'].rolling(70).agg(lambda rows: rows[0])
+            'ret1d_log':(np.log(df.adjClose) - np.log(df.yesterday)),
+            'ret3d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(3).sum(),
+            'ret7d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(7).sum(),
+            'ret14d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(14).sum(),
+            'ret30d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(30).sum(),
+            'ret60d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(60).sum(),
+            'lastclose_log':(np.log(df['adjClose']) - np.log(df['close'])),
+            'buy_queue_locked' : np.logical_and(np.isclose(df['high'], df['low']), df['high'] > df['yesterday']),
+            'sell_queue_locked' : np.logical_and(np.isclose(df['high'], df['low']), df['low'] < df['yesterday'])
         }
-    )
-
-    return return_df
-    
-    
-def ret_based_func(stock):
-    df = stock
-    return_df = pd.DataFrame({\
-                              'ret1d_log':(np.log(df.adjClose) - np.log(df.yesterday)), \
-                              'ret3d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(3).sum(), \
-                              'ret7d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(7).sum(), \
-                              'ret14d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(14).sum(), \
-                              'ret30d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(30).sum(), \
-                              'ret60d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(60).sum(), \
-                              'ret90d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(90).sum(), \
-                              'ret120d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(120).sum(), \
-                              'ret300d_log':(np.log(df.adjClose) - np.log(df.yesterday)).rolling(300).sum(), \
-                              'lastclose_log':(np.log(df['adjClose']) - np.log(df['close'])), \
-                              'buy_queue_locked' : np.logical_and(np.isclose(df['high'], df['low']), df['high'] > df['yesterday']), \
-                              'sell_queue_locked' : np.logical_and(np.isclose(df['high'], df['low']), df['low'] < df['yesterday']), \
-                              })
+)
     return return_df
 
 def prp_based_func(stock):
-    df = stock
-    df.index = [pd.Timestamp(item) for item in df['date']]
-    return_df = pd.DataFrame({\
-        'prp_high30d':df['adjClose']/df['high'].rolling('30d').max(), \
-        'prp_high60d':df['adjClose']/df['high'].rolling('60d').max(), \
-        'prp_high90d':df['adjClose']/df['high'].rolling('90d').max(), \
-        'prp_high120d':df['adjClose']/df['high'].rolling('120d').max(), \
-        'prp_high300d':df['adjClose']/df['high'].rolling('300d').max(), \
-        'prp_low30d':df['adjClose']/df['low'].rolling('30d').min(), \
-        'prp_low60d':df['adjClose']/df['low'].rolling('60d').min(), \
-        'prp_low90d':df['adjClose']/df['low'].rolling('90d').min(), \
-        'prp_low120d':df['adjClose']/df['low'].rolling('120d').min(), \
-        'prp_low300d':df['adjClose']/df['low'].rolling('300d').min(), \
-        'prp_value3d30d':df['value'].rolling('3d').mean()/df['value'].rolling('30d').mean(), \
-        'prp_value5d60d':df['value'].rolling('5d').mean()/df['value'].rolling('60d').mean(), \
-        'prp_value15d120d':df['value'].rolling('15d').mean()/df['value'].rolling('120d').mean(), \
-        'prp_value30d200d':df['value'].rolling('30d').mean()/df['value'].rolling('200d').mean(), \
-        })
+    df = stock.copy()
+
+    return_df = pd.DataFrame(
+        {
+            'prp_high30d':df['adjClose']/df['high'].rolling(30).max(),
+            'prp_high60d':df['adjClose']/df['high'].rolling(60).max(),
+            'prp_high90d':df['adjClose']/df['high'].rolling(90).max(),
+            'prp_low30d':df['adjClose']/df['low'].rolling(30).min(),
+            'prp_low60d':df['adjClose']/df['low'].rolling(60).min(),
+            'prp_low90d':df['adjClose']/df['low'].rolling(90).min(),
+            'prp_value3d30d':df['value'].rolling(3).mean()/df['value'].rolling(30).mean(),
+            'prp_value5d60d':df['value'].rolling(5).mean()/df['value'].rolling(60).mean(),
+        }
+    )
+    
     return return_df
 
 def wght_based_feature(foladHist):
@@ -132,5 +140,3 @@ def wght_based_feature(foladHist):
 
     df_feature = pd.concat([df_value20_data, df_wght_based], axis=1)
     return df_feature
-
-
--- feature_extraction/indicator_features.py
+++ feature_extraction/indicator_features.py
@@ -3,10 +3,144 @@ import numpy as np
 import pandas as pd
 import ta
 
+
+## Volatility Indicator: Bollinger Bands (BB) ##
+################################################
+def BB(stock):
+    df = stock.copy()
+
+    indicator_bb = ta.volatility.BollingerBands(df['adjClose'], window = 20, window_dev = 2)
+    df['BB_bbh'] = indicator_bb.bollinger_hband()
+    df['BB_bbl'] = indicator_bb.bollinger_lband()
+    df['last_BB_bbh'] = indicator_bb.bollinger_hband().shift(1)
+    df['last_BB_bbl'] = indicator_bb.bollinger_lband().shift(1)
+
+    # buy signal
+    df['BB_buy'] = np.nan
+    df.loc[
+        (df['last_BB_bbl'] > df['last1d_close']) & 
+        (df['last1d_open'] > df['last1d_close']) &
+        (df['BB_bbl'] > df['open']) & 
+        (df['open'] < df['close']),
+        'BB_buy' ] = 1
+    # sell signal
+    df.loc[
+        (df['last_BB_bbh'] < df['last1d_close']) & 
+        (df['last1d_open'] < df['last1d_close'])&
+        (df['BB_bbh'] < df['open']) & 
+        (df['open'] > df['close']),
+        'BB_buy' ] = 0
+
+    df['BB_position'] = df['BB_buy'].fillna(method = 'ffill').shift()
+
+    BBdf =  pd.DataFrame(
+        {
+            'BB_bbh': df['BB_bbh'],
+            'BB_bbl': df['BB_bbl'],
+            'last_BB_bbh': df['last_BB_bbh'],
+            'last_BB_bbl': df['last_BB_bbl'],
+            'BB_buy': df['BB_buy'],
+            'BB_position': df['BB_position']
+        }
+    )
+
+    return BBdf
+
+## Trend Indicator: Exponential Moving Average (EMA) ##
+#######################################################
+def EMA(stock):
+    df = stock.copy()
+
+    df['EMA_5d'] = ta.trend.ema_indicator(df['adjClose'], 5)
+    df['last_EMA_5d'] = ta.trend.ema_indicator(df['adjClose'], 5).shift(1)
+    df['EMA_40d'] = ta.trend.ema_indicator(df['adjClose'], 40)
+    df['last_EMA_40d'] = ta.trend.ema_indicator(df['adjClose'], 40).shift(1)
+
+    # buy signal
+    df['EMA_buy'] = np.nan
+
+    df.loc[(df['last_EMA_5d'] < df['last_EMA_40d']) & (df['EMA_5d'] > df['EMA_40d']), 'EMA_buy'] = 1
+    # sell signal
+    df.loc[(df['last_EMA_5d'] > df['last_EMA_40d']) & (df['EMA_5d'] < df['EMA_40d']), 'EMA_buy'] = 0
+
+    df['EMA_position'] = df['EMA_buy'].fillna(method = 'ffill').shift()
+
+    EMAdf =  pd.DataFrame(
+        {
+            'EMA_5d': df['EMA_5d'],
+            'last_EMA_5d': df['last_EMA_5d'],
+            'EMA_40d': df['EMA_40d'],
+            'last_EMA_40d': df['last_EMA_40d'],
+            'EMA_buy': df['EMA_buy'],
+            'EMA_position': df['EMA_position']
+        }
+    )
+
+    return EMAdf
+
+## Trend Indicator: Moving Average Convergence Divergence (MACD) ##
+###################################################################
+def MACD(stock):
+    df = stock.copy()
+
+    df['MACD'] = ta.trend.macd(df['adjClose'], window_slow =26,  window_fast = 12) # ta.trend.ema_indicator(df['adjClose'], 12) - ta.trend.ema_indicator(df['adjClose'], 26)
+    df['MACD_diff'] = ta.trend.macd_diff(df['adjClose'], window_slow = 26,  window_fast = 12) # df['MACD'] - df['MACD_signal']
+    df['MACD_signal'] = ta.trend.macd_signal(df['adjClose'], window_slow = 26,  window_fast = 12) # ta.trend.ema_indicator(df['MACD'], 9)
+
+    # buy signal
+    df['MACD_buy'] = np.nan
+    df.loc[(df['MACD'] < 0) & (df['MACD'] > df['MACD_signal']) & (df['MACD'].shift(1) < df['MACD_signal'].shift(1)), 'MACD_buy'] = 1
+    # sell signal
+    df.loc[(df['MACD'] > 0) & (df['MACD'] < df['MACD_signal']) & (df['MACD'].shift(1) > df['MACD_signal'].shift(1)), 'MACD_buy'] = 0
+
+    df['MACD_position'] = df['MACD_buy'].fillna(method = 'ffill').shift()
+
+    MACDdf =  pd.DataFrame(
+    {
+        'MACD': df['MACD'],
+        'MACD_diff': df['MACD_diff'],
+        'MACD_signal': df['MACD_signal'],
+        'MACD_buy': df['MACD_buy'],
+        'MACD_position': df['MACD_position']
+    }
+    )
+
+    return MACDdf
+
+## Momentum Indicator: Relative Strength Index (RSI) ##
+#######################################################
+def RSI(stock, window = 15):
+    df = stock.copy()
+
+    overBought = 70
+    overSold = 30
+
+    df['RSI'] = ta.momentum.RSIIndicator(df['adjClose'], window = window).rsi()
+    df['last_RSI'] = df['RSI'].shift(1)
+
+    # buy signal
+    df['RSI_buy'] = np.nan
+    df.loc[(df['last_RSI'] < overSold) & (df['RSI'] > overSold), 'RSI_buy'] = 1
+    # sell signal
+    df.loc[(df['last_RSI'] > overBought) & (df['RSI'] < overBought), 'RSI_buy'] = 0 
+
+    df['RSI_position'] = df['RSI_buy'].fillna(method = 'ffill').shift()
+
+
+    RSIdf =  pd.DataFrame(
+        {
+            'RSI': df['RSI'],
+            'RSI_buy': df['RSI_buy'],
+            'RSI_position': df['RSI_position']
+        }
+    )
+
+    return RSIdf
+
 ## Trend Indicator: Simple Moving Average (SMA) ##
 ##################################################
 def SMA(stock):
-    df = copy.copy(stock)
+    df = stock.copy()
 
     df['SMA10d'] = ta.trend.SMAIndicator(df['adjClose'], window = 10).sma_indicator()
     df['SMA20d'] = ta.trend.SMAIndicator(df['adjClose'], window = 20).sma_indicator()
@@ -40,105 +174,10 @@ def SMA(stock):
 
     return return_df
 
-
-# Trend Indicator: Exponential Moving Average (EMA)
-def EMA(stock):
-
-    df = stock
-
-    df['EMA_5d'] = ta.trend.ema_indicator(df['adjClose'], 5)
-    df['last_EMA_5d'] = ta.trend.ema_indicator(df['adjClose'], 5).shift(1)
-    df['EMA_40d'] = ta.trend.ema_indicator(df['adjClose'], 40)
-    df['last_EMA_40d'] = ta.trend.ema_indicator(df['adjClose'], 40).shift(1)
-
-    # buy signal
-    df['EMA_buy'] = np.nan
-
-    df.loc[(df['last_EMA_5d'] < df['last_EMA_40d']) & (df['EMA_5d'] > df['EMA_40d']), 'EMA_buy'] = 1
-    # sell signal
-    df.loc[(df['last_EMA_5d'] > df['last_EMA_40d']) & (df['EMA_5d'] < df['EMA_40d']), 'EMA_buy'] = 0
-
-    df['EMA_position'] = df['EMA_buy'].fillna(method = 'ffill').shift()
-
-    EMAdf =  pd.DataFrame(
-        {
-            'EMA_5d': df['EMA_5d'],
-            'last_EMA_5d': df['last_EMA_5d'],
-            'EMA_40d': df['EMA_40d'],
-            'last_EMA_40d': df['last_EMA_40d'],
-            'EMA_buy': df['EMA_buy'],
-            'EMA_position': df['EMA_position']
-        }
-    )
-
-    return EMAdf
-
-
-# Trend Indicator: Moving Average Convergence Divergence (MACD)
-def MACD(stock):
-
-    df = stock
-
-    df['MACD'] = ta.trend.macd(df['adjClose'], window_slow =26,  window_fast = 12) # ta.trend.ema_indicator(df['adjClose'], 12) - ta.trend.ema_indicator(df['adjClose'], 26)
-    df['MACD_diff'] = ta.trend.macd_diff(df['adjClose'], window_slow = 26,  window_fast = 12) # df['MACD'] - df['MACD_signal']
-    df['MACD_signal'] = ta.trend.macd_signal(df['adjClose'], window_slow = 26,  window_fast = 12) # ta.trend.ema_indicator(df['MACD'], 9)
-
-    # buy signal
-    df['MACD_buy'] = np.nan
-    df.loc[(df['MACD'] < 0) & (df['MACD'] > df['MACD_signal']) & (df['MACD'].shift(1) < df['MACD_signal'].shift(1)), 'MACD_buy'] = 1
-    # sell signal
-    df.loc[(df['MACD'] > 0) & (df['MACD'] < df['MACD_signal']) & (df['MACD'].shift(1) > df['MACD_signal'].shift(1)), 'MACD_buy'] = 0
-
-    df['MACD_position'] = df['MACD_buy'].fillna(method = 'ffill').shift()
-
-    MACDdf =  pd.DataFrame(
-    {
-        'MACD': df['MACD'],
-        'MACD_diff': df['MACD_diff'],
-        'MACD_signal': df['MACD_signal'],
-        'MACD_buy': df['MACD_buy'],
-        'MACD_position': df['MACD_position']
-    }
-    )
-
-    return MACDdf
-
-
-# Momentum Indicator: Relative Strength Index (RSI)
-def RSI(stock, window = 15):
-
-    df = stock
-
-    overBought = 70
-    overSold = 30
-
-    df['RSI'] = ta.momentum.RSIIndicator(df['adjClose'], window = window).rsi()
-    df['last_RSI'] = df['RSI'].shift(1)
-
-    # buy signal
-    df['RSI_buy'] = np.nan
-    df.loc[(df['last_RSI'] < overSold) & (df['RSI'] > overSold), 'RSI_buy'] = 1
-    # sell signal
-    df.loc[(df['last_RSI'] > overBought) & (df['RSI'] < overBought), 'RSI_buy'] = 0 
-
-    df['RSI_position'] = df['RSI_buy'].fillna(method = 'ffill').shift()
-
-
-    RSIdf =  pd.DataFrame(
-        {
-            'RSI': df['RSI'],
-            'RSI_buy': df['RSI_buy'],
-            'RSI_position': df['RSI_position']
-        }
-    )
-
-    return RSIdf
-
-
-# Momentum Indicator: Stochastic Oscillator (SR)
+## Momentum Indicator: Stochastic Oscillator (SR) ##
+####################################################
 def STOCHASTIC(stock):
-
-    df = stock
+    df = stock.copy()
 
     overBought = 70
     overSold = 20
@@ -175,46 +214,4 @@ def STOCHASTIC(stock):
     }
     )
     
-    return STOCHASTICdf 
-       
-
-# Volatility Indicator: Bollinger Bands (BB)
-def BB(stock):
-    df = stock
-
-    indicator_bb = ta.volatility.BollingerBands(df['adjClose'], window = 20, window_dev = 2)
-    df['BB_bbh'] = indicator_bb.bollinger_hband()
-    df['BB_bbl'] = indicator_bb.bollinger_lband()
-    df['last_BB_bbh'] = indicator_bb.bollinger_hband().shift(1)
-    df['last_BB_bbl'] = indicator_bb.bollinger_lband().shift(1)
-
-    # buy signal
-    df['BB_buy'] = np.nan
-    df.loc[
-        (df['last_BB_bbl'] > df['last_close']) & 
-        (df['last_open'] > df['last_close'])&
-        (df['BB_bbl'] > df['open']) & 
-        (df['open'] < df['close']),
-        'BB_buy' ] = 1
-    # sell signal
-    df.loc[
-        (df['last_BB_bbh'] < df['last_close']) & 
-        (df['last_open'] < df['last_close'])&
-        (df['BB_bbh'] < df['open']) & 
-        (df['open'] > df['close']),
-        'BB_buy' ] = 0
-
-    df['BB_position'] = df['BB_buy'].fillna(method = 'ffill').shift()
-
-    BBdf =  pd.DataFrame(
-        {
-            'BB_bbh': df['BB_bbh'],
-            'BB_bbl': df['BB_bbl'],
-            'last_BB_bbh': df['last_BB_bbh'],
-            'last_BB_bbl': df['last_BB_bbl'],
-            'BB_buy': df['BB_buy'],
-            'BB_position': df['BB_position']
-        }
-    )
-
-    return BBdf
+    return STOCHASTICdf
