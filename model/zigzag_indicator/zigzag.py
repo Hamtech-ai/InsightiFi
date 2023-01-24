d7fe54ad321056c9174d8a54e451748b9b88ee34
AlirezaHanifi <hanifi1375@yahoo.com>
2023-01-24 22:06:10 +0330

    some bugs fixed

---
+++ model/zigzag_indicator/zigzag.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def peak_valley_pivots(s, c):
+
+    zigzagList = []
+    zz = []
+    zigzagLabel = {}
+    signal = 0
+    inflection = s[0]
+    inflection_index = 0
+    
+    for i in range(1, len(s)):
+        # Find first trend
+        if signal == 0:
+            if np.abs(s[i] - inflection) / s[i] <= c:
+                signal = -1
+            elif np.abs(s[i] - inflection) / s[i] >= c:
+                signal = 1
+
+        # Downtrend, inflection keeps track of the lowest point in the downtrend
+        if signal == -1:
+            # New Minimum, change inflection
+            if s[i] < inflection:
+                inflection = s[i]
+                inflection_index = i
+                
+            # Trend Reversal
+            elif np.abs(s[i] - inflection) / s[i] >= c:
+                signal = 1
+                zz.append(inflection)  # Append the lowest point of the downtrend to zz
+                zigzagLabel[inflection_index] = -1
+                inflection = s[i]
+                inflection_index = i      # Current point becomes the highest point of the new uptrend
+
+        # Uptrend, inflection keeps track of the highest point in the uptrend
+        elif signal == 1:
+            # New Maximum, change inflection
+            if s[i] > inflection:
+                inflection = s[i]
+                inflection_index = i
+                
+            # Trend Reversal
+            elif np.abs(s[i] - inflection) / s[i] <= c:
+                signal = -1
+                zz.append(inflection)  # Append the highest point of the uptrend to zz
+                zigzagLabel[inflection_index] = 1
+                inflection = s[i]
+                inflection_index = i      # Current point becomes the lowest point of the new trend
+        
+    for i in range(len(s)):
+        if i not in zigzagLabel:
+            zigzagList.append(0)
+        else:
+            zigzagList.append(zigzagLabel[i])
+
+    return zigzagList
