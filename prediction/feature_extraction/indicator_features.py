99ace972022a542017ee452ca864436d368bc5b9
AlirezaHanifi <hanifi1375@yahoo.com>
2023-02-08 22:08:14 +0330

    rewritten files and corn job added

--- prediction/feature_extraction/indicator_features.py
+++ prediction/feature_extraction/indicator_features.py
@@ -4,42 +4,40 @@ import numpy as np
 import pandas as pd
 import ta
 
-
+################################################
 ## Volatility Indicator: Bollinger Bands (BB) ##
 ################################################
-def BB(stock):
-    df = stock.copy()
+def BB(ticker):
+    
+    df = ticker.copy()
 
     indicator_bb = ta.volatility.BollingerBands(df['adjClose'], window = 20, window_dev = 2)
     df['BB_bbh'] = indicator_bb.bollinger_hband()
     df['BB_bbl'] = indicator_bb.bollinger_lband()
-    df['last_BB_bbh'] = indicator_bb.bollinger_hband().shift(1)
-    df['last_BB_bbl'] = indicator_bb.bollinger_lband().shift(1)
 
-    # buy signal
+    # buy(1) or sell(0) signal?
     df['BB_buy'] = np.nan
+    # buy(1) signal
     df.loc[
-        (df['last_BB_bbl'] > df['last1d_close']) & 
-        (df['last1d_open'] > df['last1d_close']) &
+        (df['BB_bbl'].shift() > df['close'].shift()) & 
+        (df['open'].shift() > df['close'].shift()) &
         (df['BB_bbl'] > df['open']) & 
         (df['open'] < df['close']),
-        'BB_buy' ] = 1
-    # sell signal
+    'BB_buy' ] = 1
+    # sell(0) signal
     df.loc[
-        (df['last_BB_bbh'] < df['last1d_close']) & 
-        (df['last1d_open'] < df['last1d_close'])&
+        (df['BB_bbh'].shift() < df['close'].shift()) & 
+        (df['open'].shift() < df['close'].shift()) &
         (df['BB_bbh'] < df['open']) & 
         (df['open'] > df['close']),
-        'BB_buy' ] = 0
+    'BB_buy' ] = 0
 
-    df['BB_position'] = df['BB_buy'].fillna(method = 'ffill').shift()
+    df['BB_position'] = df['BB_buy'].fillna(method = 'ffill')
 
     return_df =  pd.DataFrame(
         {
             'BB_bbh': df['BB_bbh'],
             'BB_bbl': df['BB_bbl'],
-            'last_BB_bbh': df['last_BB_bbh'],
-            'last_BB_bbl': df['last_BB_bbl'],
             'BB_buy': df['BB_buy'],
             'BB_position': df['BB_position']
         }
@@ -47,30 +45,40 @@ def BB(stock):
 
     return return_df
 
+#######################################################
 ## Trend Indicator: Exponential Moving Average (EMA) ##
 #######################################################
-def EMA(stock):
-    df = stock.copy()
+def EMA(ticker):
+
+    df = ticker.copy()
 
     df['EMA_5d'] = ta.trend.ema_indicator(df['adjClose'], 5)
     df['last_EMA_5d'] = ta.trend.ema_indicator(df['adjClose'], 5).shift(1)
     df['EMA_40d'] = ta.trend.ema_indicator(df['adjClose'], 40)
     df['last_EMA_40d'] = ta.trend.ema_indicator(df['adjClose'], 40).shift(1)
 
-    # buy signal
+    # buy(1) or sell(0) signal?
     df['EMA_buy'] = np.nan
+    # buy(1) signal
+    df.loc[
+        (df['last_EMA_5d'] < df['last_EMA_40d']) & 
+        (df['EMA_5d'] > df['EMA_40d']), 
+    'EMA_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['last_EMA_5d'] > df['last_EMA_40d']) & 
+        (df['EMA_5d'] < df['EMA_40d']), 
+    'EMA_buy'] = 0
 
-    df.loc[(df['last_EMA_5d'] < df['last_EMA_40d']) & (df['EMA_5d'] > df['EMA_40d']), 'EMA_buy'] = 1
-    # sell signal
-    df.loc[(df['last_EMA_5d'] > df['last_EMA_40d']) & (df['EMA_5d'] < df['EMA_40d']), 'EMA_buy'] = 0
-
-    df['EMA_position'] = df['EMA_buy'].fillna(method = 'ffill').shift()
+    df['EMA_position'] = df['EMA_buy'].fillna(method = 'ffill')
 
     return_df =  pd.DataFrame(
         {
             'EMA_5d': df['EMA_5d'],
+            'EMA_5d_diff': df['adjClose'] - df['EMA_5d'],
             'last_EMA_5d': df['last_EMA_5d'],
             'EMA_40d': df['EMA_40d'],
+            'EMA_40d_diff': df['adjClose'] - df['EMA_40d'],
             'last_EMA_40d': df['last_EMA_40d'],
             'EMA_buy': df['EMA_buy'],
             'EMA_position': df['EMA_position']
@@ -79,54 +87,146 @@ def EMA(stock):
 
     return return_df
 
+####################################################
+## Trend Indicator: Ichimoku Kinkō Hyō (Ichimoku) ##
+####################################################
+def ICHIMOKU(ticker):
+
+    df = ticker.copy()
+
+    df['Tenkan_Sen'] = ta.trend.ichimoku_conversion_line(df['high'], df['low']) # AKA: Turning Line
+    df['Kijun_Sen'] = ta.trend.ichimoku_base_line(df['high'], df['low']) # AKA: Standard Line
+    df['Chikou_Span']  = df['adjClose'].shift(-26) # AKA: Lagging Line
+    df['Senkou_Span_A'] = ta.trend.ichimoku_a(df['high'], df['low']) # AKA: 1st Leading Line
+    df['Senkou_Span_B'] = ta.trend.ichimoku_b(df['high'], df['low']) # AKA: 2nd Leading Line
+
+    # buy(1) or sell(0) signal? (intersection Tenkan Sen and Kijun Sen)
+    df['Tenkan_Kijun_cross_buy'] = np.nan
+    # buy(1) signal
+    df.loc[
+        (df['Tenkan_Sen'].shift() < df['Kijun_Sen'].shift()) & 
+        (df['Tenkan_Sen'] > df['Kijun_Sen']), 
+    'Tenkan_Kijun_cross_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['Tenkan_Sen'].shift() > df['Kijun_Sen'].shift()) & 
+        (df['Tenkan_Sen'] < df['Kijun_Sen']), 
+    'Tenkan_Kijun_cross_buy'] = 0
+
+    df['Tenkan_Kijun_cross_position'] = df['Tenkan_Kijun_cross_buy'].fillna(method = 'ffill')
+
+    # buy(1) or sell(0) signal? (intersection Senkou Span A and Senkou Span A)
+    df['Senkou_cross_buy'] = np.nan
+    # buy(1) signal
+    df.loc[
+        (df['Senkou_Span_A'].shift() < df['Senkou_Span_B'].shift()) & 
+        (df['Senkou_Span_A'] > df['Senkou_Span_B']), 
+    'Senkou_cross_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['Senkou_Span_A'].shift() > df['Senkou_Span_B'].shift()) & 
+        (df['Senkou_Span_A'] < df['Senkou_Span_B']), 
+    'Senkou_cross_buy'] = 0
+
+    df['Senkou_cross_position'] = df['Senkou_cross_buy'].fillna(method = 'ffill')
+
+    # buy(1) or sell(0) signal? (intersection Chikou Span and adjClose)
+    df['Chikou_adjClose_cross_buy'] = np.nan
+    # buy(1) signal
+    df.loc[
+        (df['Chikou_Span'].shift() < df['adjClose'].shift()) & 
+        (df['Chikou_Span'] > df['adjClose']), 
+    'Chikou_adjClose_cross_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['Chikou_Span'].shift() > df['adjClose'].shift()) & 
+        (df['Chikou_Span'] < df['adjClose']), 
+    'Chikou_adjClose_cross_buy'] = 0
+
+    df['Chikou_adjClose_cross_position'] = df['Chikou_adjClose_cross_buy'].fillna(method = 'ffill')
+
+
+    return_df =  pd.DataFrame(
+        {
+            'Tenkan_Sen': df['Tenkan_Sen'],
+            'Kijun_Sen': df['Kijun_Sen'],
+            'Chikou_Span': df['Chikou_Span'],
+            'Senkou_Span_A': df['Senkou_Span_A'],
+            'Senkou_Span_B': df['Senkou_Span_B'],
+            'Tenkan_Kijun_cross_position': df['Tenkan_Kijun_cross_position'],
+            'Senkou_cross_position': df['Senkou_cross_position'],
+            'Chikou_adjClose_cross_position': df['Chikou_adjClose_cross_position']
+        }
+    )
+
+    return return_df
+
+###################################################################
 ## Trend Indicator: Moving Average Convergence Divergence (MACD) ##
 ###################################################################
-def MACD(stock):
-    df = stock.copy()
+def MACD(ticker):
+
+    df = ticker.copy()
 
     df['MACD'] = ta.trend.macd(df['adjClose'], window_slow =26,  window_fast = 12) # ta.trend.ema_indicator(df['adjClose'], 12) - ta.trend.ema_indicator(df['adjClose'], 26)
     df['MACD_diff'] = ta.trend.macd_diff(df['adjClose'], window_slow = 26,  window_fast = 12) # df['MACD'] - df['MACD_signal']
     df['MACD_signal'] = ta.trend.macd_signal(df['adjClose'], window_slow = 26,  window_fast = 12) # ta.trend.ema_indicator(df['MACD'], 9)
 
-    # buy signal
+    # buy(1) or sell(0) signal?
     df['MACD_buy'] = np.nan
-    df.loc[(df['MACD'] < 0) & (df['MACD'] > df['MACD_signal']) & (df['MACD'].shift(1) < df['MACD_signal'].shift(1)), 'MACD_buy'] = 1
-    # sell signal
-    df.loc[(df['MACD'] > 0) & (df['MACD'] < df['MACD_signal']) & (df['MACD'].shift(1) > df['MACD_signal'].shift(1)), 'MACD_buy'] = 0
+    # buy(1) signal
+    df.loc[
+        (df['MACD'] < 0) & 
+        (df['MACD'] > df['MACD_signal']) &
+        (df['MACD'].shift(1) < df['MACD_signal'].shift(1)), 
+    'MACD_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['MACD'] > 0) &
+        (df['MACD'] < df['MACD_signal']) &
+        (df['MACD'].shift(1) > df['MACD_signal'].shift(1)), 
+    'MACD_buy'] = 0
 
-    df['MACD_position'] = df['MACD_buy'].fillna(method = 'ffill').shift()
+    df['MACD_position'] = df['MACD_buy'].fillna(method = 'ffill')
 
     return_df =  pd.DataFrame(
-    {
-        'MACD': df['MACD'],
-        'MACD_diff': df['MACD_diff'],
-        'MACD_signal': df['MACD_signal'],
-        'MACD_buy': df['MACD_buy'],
-        'MACD_position': df['MACD_position']
-    }
+        {
+            'MACD': df['MACD'],
+            'MACD_diff': df['MACD_diff'],
+            'MACD_signal': df['MACD_signal'],
+            'MACD_buy': df['MACD_buy'],
+            'MACD_position': df['MACD_position']
+        }
     )
 
     return return_df
 
+#######################################################
 ## Momentum Indicator: Relative Strength Index (RSI) ##
 #######################################################
-def RSI(stock, window = 15):
-    df = stock.copy()
+def RSI(ticker, window = 15):
 
+    df = ticker.copy()
     overBought = 70
     overSold = 30
 
     df['RSI'] = ta.momentum.RSIIndicator(df['adjClose'], window = window).rsi()
     df['last_RSI'] = df['RSI'].shift(1)
 
-    # buy signal
+    # buy(1) or sell(0) signal?
     df['RSI_buy'] = np.nan
-    df.loc[(df['last_RSI'] < overSold) & (df['RSI'] > overSold), 'RSI_buy'] = 1
-    # sell signal
-    df.loc[(df['last_RSI'] > overBought) & (df['RSI'] < overBought), 'RSI_buy'] = 0 
+    # buy(1) signal
+    df.loc[
+        (df['last_RSI'] < overSold) &
+        (df['RSI'] > overSold), 
+    'RSI_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['last_RSI'] > overBought) &
+        (df['RSI'] < overBought), 
+    'RSI_buy'] = 0 
 
-    df['RSI_position'] = df['RSI_buy'].fillna(method = 'ffill').shift()
-
+    df['RSI_position'] = df['RSI_buy'].fillna(method = 'ffill')
 
     return_df =  pd.DataFrame(
         {
@@ -138,10 +238,12 @@ def RSI(stock, window = 15):
 
     return return_df
 
+##################################################
 ## Trend Indicator: Simple Moving Average (SMA) ##
 ##################################################
-def SMA(stock):
-    df = stock.copy()
+def SMA(ticker):
+
+    df = ticker.copy()
 
     df['SMA10d'] = ta.trend.SMAIndicator(df['adjClose'], window = 10).sma_indicator()
     df['SMA20d'] = ta.trend.SMAIndicator(df['adjClose'], window = 20).sma_indicator()
@@ -151,13 +253,20 @@ def SMA(stock):
     df['last_SMA50d'] = ta.trend.SMAIndicator(df['adjClose'], window = 50).sma_indicator().shift(1)
     df['SMA80d'] = ta.trend.SMAIndicator(df['adjClose'], window = 80).sma_indicator()
 
-    # buy signal
+    # buy(1) or sell(0) signal?
     df['RSI_buy'] = np.nan
-    df.loc[(df['last_SMA20d'] < df['last_SMA50d']) & (df['SMA20d'] > df['SMA50d']), 'SMA_buy'] = 1
-    # sell signal
-    df.loc[(df['last_SMA20d'] > df['last_SMA50d']) & (df['SMA20d'] < df['SMA50d']), 'SMA_buy'] = 0
+    # buy(1) signal
+    df.loc[
+        (df['last_SMA20d'] < df['last_SMA50d']) & 
+        (df['SMA20d'] > df['SMA50d']), 
+    'SMA_buy'] = 1
+    # sell(0) signal
+    df.loc[
+        (df['last_SMA20d'] > df['last_SMA50d']) & 
+        (df['SMA20d'] < df['SMA50d']), 
+    'SMA_buy'] = 0
 
-    df['SMA_position'] = df['SMA_buy'].fillna(method = 'ffill').shift()
+    df['SMA_position'] = df['SMA_buy'].fillna(method = 'ffill')
 
     return_df =  pd.DataFrame(
         {
@@ -175,11 +284,12 @@ def SMA(stock):
 
     return return_df
 
+####################################################
 ## Momentum Indicator: Stochastic Oscillator (SR) ##
 ####################################################
-def STOCHASTIC(stock):
-    df = stock.copy()
+def STOCHASTIC(ticker):
 
+    df = ticker.copy()
     overBought = 70
     overSold = 20
 
@@ -188,31 +298,32 @@ def STOCHASTIC(stock):
     df['last_STOCH_fast'] = ta.momentum.StochasticOscillator(df['high'], df['low'], df['close'], 15, 5).stoch().shift(1)
     df['last_STOCH_slow'] = ta.momentum.StochasticOscillator(df['high'], df['low'], df['close'], 15, 5).stoch_signal().shift(1)
 
-    # buy signal
+    # buy(1) or sell(0) signal?
     df['STOCH_buy'] = np.nan
+    # buy(1) signal
     df.loc[
         (df['STOCH_fast'] > overSold) &
         (df['last_STOCH_fast'] < df['last_STOCH_slow']) &
         (df['STOCH_slow'] < df['STOCH_fast']), 
-        'STOCH_buy'] = 1
-    # sell signal
+    'STOCH_buy'] = 1
+    # sell(0) signal
     df.loc[
         (df['STOCH_fast'] > overBought) &
         (df['last_STOCH_fast'] > df['last_STOCH_slow']) &
         (df['STOCH_slow'] > df['STOCH_fast']), 
-        'STOCH_buy'] = 0 
+    'STOCH_buy'] = 0 
 
     df['STOCH_position'] = df['STOCH_buy'].fillna(method = 'ffill').shift()
 
     return_df =  pd.DataFrame(
-    {
-        'STOCH_fast': df['STOCH_fast'],
-        'STOCH_slow': df['STOCH_slow'],
-        'last_STOCH_fast': df['last_STOCH_fast'],
-        'last_STOCH_slow': df['last_STOCH_slow'],
-        'STOCH_buy': df['STOCH_buy'],
-        'STOCH_position': df['STOCH_position']
-    }
+        {
+            'STOCH_fast': df['STOCH_fast'],
+            'STOCH_slow': df['STOCH_slow'],
+            'last_STOCH_fast': df['last_STOCH_fast'],
+            'last_STOCH_slow': df['last_STOCH_slow'],
+            'STOCH_buy': df['STOCH_buy'],
+            'STOCH_position': df['STOCH_position']
+        }
     )
     
     return return_df
--- prediction/run_all.py
+++ prediction/run_all.py
@@ -10,8 +10,9 @@ from sklearn.model_selection import train_test_split
 from zigzag_indicator.zigzag import peak_valley_pivots
 
 warnings.filterwarnings('ignore')
-from feature_extraction.daily_features import *
 from feature_extraction.indicator_features import *
+from feature_extraction.price_features import *
+from feature_extraction.trader_features import *
 from models.model import *
 
 #########################
@@ -24,42 +25,42 @@ def runModel():
         write_to_csv = True, 
         include_jdate = True
     )
-    folad = tse.Ticker(
+    ticker = tse.Ticker(
         symbol = 'فولاد',  
         adjust = True
     )
-    foladHist = folad.history
-    foladClient = folad.client_types
+    ticker_history = ticker.history
 
-    foladHist = foladHist[foladHist.columns[:-1].insert(0, foladHist.columns[-1]).to_list()] # change location of jdate
+    ticker_history = ticker_history[ticker_history.columns[:-1].insert(0, ticker_history.columns[-1]).to_list()] # change location of jdate
 
-    ## Getting client data ##
+    ## Getting traders data ##
     #########################
 
-    foladClient = foladClient.iloc[::-1]
-    foladClient.reset_index(
+    ticker_traders_types = ticker.client_types
+    ticker_traders_types = ticker_traders_types.iloc[::-1]
+    ticker_traders_types.reset_index(
         drop = True,
         inplace = True
     )
-    foladClient['date'] = foladClient['date'].apply(
+    ticker_traders_types['date'] = ticker_traders_types['date'].apply(
         lambda x: datetime.datetime.strptime(x, '%Y%m%d')
     )
-    foladClient.iloc[:, 1:] = foladClient.iloc[:, 1:].astype('float')
+    ticker_traders_types.iloc[:, 1:] = ticker_traders_types.iloc[:, 1:].astype('float')
 
     ## Getting TEDPIX data ##
     #########################
 
-    marketIndex = download_financial_indexes(
+    market_index = download_financial_indexes(
         symbols = 'شاخص كل', 
         write_to_csv = True
     )
-    marketIndex = marketIndex['شاخص كل']
-    marketIndex.rename(
+    market_index = market_index['شاخص كل']
+    market_index.rename(
         columns = {'value': 'TEDPIX'}, 
         inplace = True
     )
-    foladHist = foladHist[foladHist.columns].merge(
-        marketIndex, 
+    ticker_history = ticker_history.merge(
+        market_index, 
         how = 'left', 
         on = 'date'
     )
@@ -67,68 +68,77 @@ def runModel():
     ## Extracting features ##
     #########################
 
-    calCols = calender_features(foladHist)
-    candelCols = candlestick_feature(foladHist)
-    prpCols = prp_based(foladHist)
-    retCols = ret_based(foladHist)
-    shiftCols = shift_data(foladHist)
-    wghtCols = weight_feature(foladHist)
+    calenderFeatures = calender_features(ticker_history)
+    candelFeatures = candlestick_feature(ticker_history)
+    prpFeatures = prp_based(ticker_history)
+    retFeatures = ret_based(ticker_history)
+    shiftFeatures = shift_data(ticker_history)
+    wghtFeatures = weight_feature(ticker_history)
 
     priceFeatures = pd.concat(
         [
-            foladHist,
-            calCols,
-            candelCols,
-            prpCols,
-            retCols,
-            shiftCols,
-            wghtCols
+            ticker_history,
+            calenderFeatures,
+            candelFeatures,
+            prpFeatures,
+            retFeatures,
+            shiftFeatures,
+            wghtFeatures
         ], axis = 1
     )
 
-    priceFeatures = priceFeatures.merge(
-        indv_nonindv_features(foladClient),
+    traderFeatures = ticker_traders_types.merge(
+        indv_nonindv_features(ticker_traders_types),
         how = 'left',
         on = 'date'
     )
 
-    bbCols = BB(priceFeatures)
-    emaCols = EMA(foladHist)
-    macdCols = MACD(foladHist)
-    smaCols = SMA(foladHist)
-    stochasticCols = STOCHASTIC(foladHist)
-    rsiCols = RSI(foladHist)
+    bbFeatures = BB(ticker_history)
+    ichiFeatures = ICHIMOKU(ticker_history)
+    emaFeatures = EMA(ticker_history)
+    macdFeatures = MACD(ticker_history)
+    smaFeatures = SMA(ticker_history)
+    stochasticFeatures = STOCHASTIC(ticker_history)
+    rsiFeatures = RSI(ticker_history)
 
     indicatorFeatures = pd.concat(
         [
-            bbCols,
-            emaCols,
-            macdCols,
-            smaCols,
-            stochasticCols,
-            rsiCols,
+            bbFeatures,
+            ichiFeatures,
+            emaFeatures,
+            macdFeatures,
+            smaFeatures,
+            stochasticFeatures,
+            rsiFeatures,
         ], axis = 1
     )
 
-    features = pd.concat(
+
+    features_extracted = pd.concat(
         [
             priceFeatures,
             indicatorFeatures
         ],
         axis = 1
     )
+    features_extracted = features_extracted.merge(
+        traderFeatures,
+        how = 'right',
+        on = 'date'
+    )
 
-    features.dropna(
+    features_extracted.dropna(
         axis = 'columns', 
-        thresh = len(features) - 450,
+        thresh = len(features_extracted) - 385,
         inplace = True
     )
 
-    features.dropna(
+    features_extracted.dropna(
         axis = 'index', 
         how = 'any',
         inplace = True
     )
+
 
 
     ## Labeling data with zigzag ##
@@ -136,7 +146,7 @@ def runModel():
 
     pivots = pd.DataFrame(
         peak_valley_pivots(
-            foladHist['adjClose'].to_list(), 0.1
+            ticker_history['adjClose'].to_list(), 0.1
         ) ,
         columns = ['label']
     ) * -1
@@ -155,10 +165,10 @@ def runModel():
     )
 
     pivots.columns = ['pivots']
-    signals['date'] = foladHist['date']
-    pivots['date'] = foladHist['date']
+    signals['date'] = ticker_history['date']
+    pivots['date'] = ticker_history['date']
 
-    initFeatures = features.merge(
+    initFeatures = features_extracted.merge(
         signals,
         how = 'left',
         on = 'date'
@@ -186,7 +196,7 @@ def runModel():
         output.to_csv('../api/inputs/output_for_API.csv', index = False) # if code run without docker
     except: 
         output.to_csv('output_for_API.csv', index = False) # if code run in docker
-    
+    print('alireza')
     return output
 
 #########################
